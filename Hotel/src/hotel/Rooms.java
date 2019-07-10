ef45fbba4449da29ba65992622e747e8d3a6d287
lauraboekling <laura.boekling@capgemini.com>
2019-07-10 16:10:25 +0200

    worked on the room class, added enums for bedtype and roomtype

--- Hotel/src/hotel/Rooms.java
+++ Hotel/src/hotel/Rooms.java
@@ -1,4 +1,25 @@
 package hotel;
 
 public class Rooms {
+
+    int roomNumber;
+    int floor;
+    RoomType type;
+    int adults;
+    int children;
+    BedType bedType;
+    boolean isDisabled;
+
+    double price;
+    boolean isAvailable;
+
+    public Rooms(int roomNumber, int floor, RoomType type, int adults, int children, BedType bedType, boolean isDisabled) {
+        this.roomNumber = roomNumber;
+        this.floor = floor;
+        this.type = type;
+        this.adults = adults;
+        this.children = children;
+        this.bedType = bedType;
+        this.isDisabled = isDisabled;
+    }
 }
