9240fb596d68b739dedd91d006ae8bc8ff6fd155
CEEJANSE <cees.a.jansen@capgemini.com>
2019-07-10 12:58:18 +0200

    Added some initial 'routes' for the console app.

--- Hotel/src/hotel/App.java
+++ Hotel/src/hotel/App.java
@@ -5,7 +5,10 @@ public class App {
     public static void main(String[] args) {
         Terminal console = new Terminal();
         String welcomeScreen = "Welcome to the hotel, please select an option: \n" +
-                "1: Look for rooms.";
+                "1: Look for rooms. \n" +
+                "2: Check booking. \n" +
+                "3: Log in. \n" +
+                "4: Contact information. \n";
         console.startTerminal(null,welcomeScreen);
     }
 }
