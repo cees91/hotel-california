f74d7a9db4405f6e9a26ea02e96460f26f5c0503
lauraboekling <laura.boekling@capgemini.com>
2019-07-11 12:54:48 +0200

    worked on classes employee guest rooms

--- Hotel/src/hotel/Employee.java
+++ Hotel/src/hotel/Employee.java
@@ -6,4 +6,25 @@ public class Employee extends User {
     String employeeId;
     String accessLevel;
 
+    public Employee(User user, String employeeId, String accessLevel) {
+        this.user = user;
+        this.employeeId = employeeId;
+        this.accessLevel = accessLevel;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+        this.accessLevel = accessLevel;
+    }
 }
