15aaf864d57d6ef8548cc6db7216f8d9454e63d3
Cees <cees.a.jansen@capgemini.com>
2019-07-18 13:36:52 +0200

    added test directory

--- jUnit-test/Application/controllers/BookingControllerTest.java
+++ jUnit-test/Application/controllers/BookingControllerTest.java
@@ -1,5 +1,32 @@
+package Application.controllers;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BookingControllerTest {
 
+    @Test
+    public void getBooking() {
+    }
+
+    @Test
+    public void getBookingById() {
+    }
+
+    @Test
+    public void createBooking() {
+    }
+
+    @Test
+    public void showBookings() {
+    }
+
+    @Test
+    public void specifyGuestsAndDates() {
+    }
+
+    @Test
+    public void createAndSaveBooking() {
+    }
 }
